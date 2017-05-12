cc719ee07d5659de5477935ebaf26dcef3e31406
vitordelfino <vitor.delfino952@gmail.com>
2017-05-11 22:43:34 -0300

    msg

--- src/main/java/br/com/sppd/factory/ConnectionFactory.java
+++ src/main/java/br/com/sppd/factory/ConnectionFactory.java
@@ -23,9 +23,9 @@ public class ConnectionFactory {
 		try{
 			String driveName = "com.mysql.jdbc.Driver";
 			Class.forName(driveName);		
-			String url = "jdbc:mysql://localhost/sppd_tg";
-			String userName = "root";
-			String passWord = "123456";
+			String url = "jdbc:mysql://bd-sppd.mysql.uhserver.com/bd_sppd";
+			String userName = "vitor_adm";
+			String passWord = "224468.Delfino";
 			
 			con = DriverManager.getConnection(url,userName,passWord);
 			
